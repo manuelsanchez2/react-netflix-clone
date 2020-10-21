45599ab869448f201e37b0463b77aed76dec2431
manuelsanchez2 <manusansan22@gmail.com>
2020-10-21 15:44:23 +0200

    Fetch pictures

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,10 @@
-import Axios from "axios";
 import React, { useState, useEffect } from "react";
 import axios from "../api/axios";
 
 const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
 
+  const base_url = "https://image.tmdb.org/t/p/w500/";
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -14,9 +14,17 @@ const Row = ({ title, fetchUrl }) => {
     fetchData();
   }, [fetchUrl]);
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
-      {/* {containers here} */}
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 };
